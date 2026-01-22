e7b8c8be1196bbcc040bd8811d21641ae6a6cfcf
abrihaye <alexandre.brihaye@hotmail.com>
2026-01-21 19:17:58 -0500

    updated server.js to add

--- server.js
+++ server.js
@@ -2,17 +2,11 @@ const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     if (req.url === '/budget') {
-        let data = "[]";
-        if (fs.existsSync('ledger.json'))
-        {
-            console.log("ledger.json exists");
-            data = fs.readFileSync('ledger.json');
-        }
         switch (req.method){
             case 'OPTIONS':
                 res.writeHead(204);
@@ -21,21 +15,34 @@ const server = http.createServer((req, res) => {
             case 'GET':
                 console.log("localhost:3000/budget GET received");
                 res.setHeader('Content-Type', 'application/json');
+                let data = "[]";
+                if (fs.existsSync('ledger.json'))
+                {
+                    console.log("ledger.json exists");
+                    data = fs.readFileSync('ledger.json');
+                }
                 res.end(data);
                 break;
             case 'POST':
                 let body = '';
                 console.log("localhost:3000/budget POST received");
-
                 req.on('data', (chunk) => {
                     body += chunk.toString();
                 })
                 req.on('end', () => {
-                    console.log(body);
+                    const newItem = JSON.parse(body);
+                    
+                    let currentLedger = [];
+                    if (fs.existsSync('ledger.json')) {
+                        currentLedger = JSON.parse(fs.readFileSync('ledger.json'));
+                    }
+
+                    currentLedger.push(newItem);
+                    const data = JSON.stringify(currentLedger, null, 2);
+                    fs.writeFileSync('ledger.json', data);
+
+                    res.end(JSON.stringify({status : "Saved"}));
                 })
-                // Create the file
-                //data.push(req.)
-                //fs.writeFileSync('ledger.json', data);
                 break;
             default:
                 console.log("Sorry, out of method range");
